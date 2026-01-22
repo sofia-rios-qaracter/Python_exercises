61588b5dac9869d063056601711cafbf81290898
Sofia <sofia.rios@qaracter.com>
2026-01-22 09:48:34 +0000

    Fixed calling to the correct function

--- Iterating_things_Exercises/Iterating_strings_exercise_1.py
+++ Iterating_things_Exercises/Iterating_strings_exercise_1.py
@@ -8,13 +8,13 @@ def count_vocals(word):
 
 def count_vocals_with_if(word):
     total_of_vocals = 0
-    for character in word:
+    for character in word.lower():
         if character == 'a' or character == 'e' or character == 'i' or character == 'o' or character == 'u':
             total_of_vocals += 1
 
     return total_of_vocals
 
-word = "Esternocleidomastoideo"
+word = "EsternocleidomAstoidEo"
 
 print(f"Vocals in the word {word}:\t\t{count_vocals(word)}")
-print(f"Vocals in the word {word} using ifs:\t{count_vocals(word)}")
+print(f"Vocals in the word {word} using ifs:\t{count_vocals_with_if(word)}")
