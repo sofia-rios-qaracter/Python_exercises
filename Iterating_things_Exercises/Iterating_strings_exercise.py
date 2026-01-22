ffe3c05a70e5dd8c5d3e018f24923158da9ce7d6
Sofia <sofia.rios@qaracter.com>
2026-01-22 09:41:44 +0000

    Added count vocals with ifs option

--- Iterating_things_Exercises/Iterating_strings_exercise.py
+++ Iterating_things_Exercises/Iterating_strings_exercise.py
@@ -6,6 +6,15 @@ def count_vocals(word):
     
     return total_of_vocals
 
+def count_vocals_with_if(word):
+    total_of_vocals = 0
+    for character in word:
+        if character == 'a' or character == 'e' or character == 'i' or character == 'o' or character == 'u':
+            total_of_vocals += 1
+
+    return total_of_vocals
+
 word = "Esternocleidomastoideo"
 
-print(f"Vocals in the word {word}: {count_vocals(word)}")
+print(f"Vocals in the word {word}:\t\t{count_vocals(word)}")
+print(f"Vocals in the word {word} using ifs:\t{count_vocals(word)}")
