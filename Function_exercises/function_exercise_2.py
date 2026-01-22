62ef93bedf8e5702606aedad5ecb4fe68e0df2d6
Sofia <sofia.rios@qaracter.com>
2026-01-22 12:35:08 +0000

    Function exercise 2

---
+++ Function_exercises/function_exercise_2.py
@@ -0,0 +1,12 @@
+def sum_temperatures(temperatures):
+    return sum(temperatures)
+
+temperatures = [20, 25, 28, 15, 30, -4, 0, 45, 30]
+sum_of_temperatures = sum_temperatures(temperatures)
+
+print("="*30)
+
+print(f"Temperatures: {temperatures}")
+print(f"Sum of the temperatures: {sum_of_temperatures}")
+
+print("="*30)
